5906e8b922531923a3380162fd9c5f07fdc8497b
Facundo Galán <facundogalan91@gmail.com>
2018-02-24 04:10:48 -0300

    Update extended greatest common divisor algorithm.

--- math/exgcd.cpp
+++ math/exgcd.cpp
@@ -15,8 +15,37 @@ T exgcd (T a, T b, T& x, T& y) {
 	return d;
 }
 
+template <typename T>
+void makeXPositive(T a, T b, T& x, T& y, T gcd){
+	if (x > 0) return;
+	T m = (T)ceil((-x / double(b / gcd)));
+	x += ((b / gcd) * m);
+	y -= ((a / gcd) * m);
+}
+
+template <typename T>
+void makeYPositive(T a, T b, T& x, T& y, T gcd){
+	if (y > 0 ) return;
+	T m = (T)ceil((-y / double(a/ gcd)));
+	x -= ((b / gcd) * m);
+	y += ((a / gcd) * m);
+}
+
 int main () {
-	int x, y, a = 21, b = 35;
-	std::cout << exgcd(a, b, x, y) << std::endl;
+	int x, y, a = 21, b = 35, c = 56, gcd;
+	
+	gcd = exgcd(a, b, x, y);
+	
+	std::cout << gcd << std::endl;
+	
+	x *= (c / gcd); y *= (c / gcd);
 	std::cout << x << " " << y << std::endl;
+	
+	makeXPositive(a, b, x, y, gcd);
+	std::cout << x << " " << y << std::endl;
+	
+	makeYPositive(a, b, x, y, gcd);
+	std::cout << x << " " << y << std::endl;
+	
+	return 0;
 }
