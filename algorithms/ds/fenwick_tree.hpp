edcdad7943cbd9be3ff6b0cf80d8f507bf23a9c6
Facundo Galan <facundogalan91@gmail.com>
2022-12-01 02:04:00 -0300

    Fix FenwickTree2DPointUpdateRangeQuery

--- algorithms/ds/fenwick_tree.hpp
+++ algorithms/ds/fenwick_tree.hpp
@@ -8,6 +8,7 @@
 
 /**
  * \brief Defines a class for a Fenwick Tree (Binary Indexed Tree).
+ *        State: tested.
  *
  * \tparam ValueType Type of the elements.
  * \tparam MaxSize Maximum number of elements.
@@ -16,13 +17,13 @@ template<typename ValueType, int MaxSize>
 class FenwickTreePointUpdateRangeQuery {
 private:
 	int n;
-	ValueType bit[MaxSize];
+	ValueType bit[MaxSize + 1];
 
 public:
 
 	void init(int size) {
 		n = size + 1;
-		std::fill(bit, bit + n, 0);
+		memset(bit, 0, sizeof(ValueType) * n);
 	}
 
 	void init(int size, ValueType* arr) {
@@ -54,6 +55,7 @@ public:
 
 /**
  * \brief Defines a class for a Fenwick Tree (Binary Indexed Tree).
+ *        State: untested.
  *
  * \tparam ValueType Type of the elements.
  * \tparam MaxSize Maximum number of elements.
@@ -62,7 +64,7 @@ template<typename ValueType, int MaxSize>
 class FenwickTreeRangeUpdatePointQuery {
 private:
 	int n;
-	ValueType bit[MaxSize];
+	ValueType bit[MaxSize + 2];
 
 	void add(int idx, ValueType delta) {
 		for (++idx; idx < n; idx += idx & -idx)
@@ -72,8 +74,8 @@ private:
 public:
 
 	void init(int size) {
-		n = size + 1;
-		std::fill(bit, bit + n, 0);
+		n = size + 2;
+		memset(bit, 0, sizeof(ValueType) * n);
 	}
 
 	void range_add(int l, int r, ValueType val) {
@@ -91,6 +93,7 @@ public:
 
 /**
  * \brief Defines a class for a Fenwick Tree (Binary Indexed Tree).
+ *        State: untested.
  *
  * \tparam ValueType Type of the elements.
  * \tparam MaxSize Maximum number of elements.
@@ -125,6 +128,7 @@ public:
 
 /**
  * \brief Defines a class for a Fenwick Tree 2D (Binary Indexed Tree 2D).
+ *        State: tested.
  *
  * \tparam ValueType Type of the elements.
  * \tparam MaxRows Maximum number of rows.
@@ -134,7 +138,7 @@ template<typename ValueType, int MaxRows, int MaxCols>
 class FenwickTree2DPointUpdateRangeQuery {
 private:
 	int n, m;
-	ValueType bit[MaxRows][MaxCols];
+	ValueType bit[MaxRows + 1][MaxCols + 1];
 
 public:
 
@@ -142,38 +146,38 @@ public:
 		n = rows + 1;
 		m = cols + 1;
 		for (int i = 0; i < n; i++)
-			std::fill(bit[i], bit[i] + m, 0);
+			memset(bit[i], 0, sizeof(ValueType) * m);
 	}
 
 	void init(int rows, int cols, ValueType** mat) {
 		init(rows, cols);
 		for (size_t i = 0; i < rows; i++)
 			for (size_t j = 0; j < cols; j++)
-				add(i, mat[i][j]);
+				add(i, j, mat[i][j]);
 	}
 
 	void init(std::vector<std::vector<ValueType>>& mat) {
 		init(mat.size(), mat[0].size());
 		for (size_t i = 0; i < mat.size(); i++)
 			for (size_t j = 0; j < mat[i].size(); j++)
-				add(i, mat[i][j]);
+				add(i, j, mat[i][j]);
 	}
 
-	void add(int x, int y, ValueType delta) {
-		for (int i = x + 1; i > 0; i -= i & -i)
-			for (int j = y + 1; j > 0; j -= j & -j)
+	void add(int row, int col, ValueType delta) {
+		for (int i = row + 1; i < n; i += i & -i)
+			for (int j = col + 1; j < m; j += j & -j)
 				bit[i][j] += delta;
 	}
 
-	ValueType sum(int x, int y) {
+	ValueType sum(int row, int col) {
 		ValueType ret = 0;
-		for (int i = x + 1; i > 0; i -= i & -i)
-			for (int j = y + 1; j > 0; j -= j & -j)
+		for (int i = row + 1; i > 0; i -= i & -i)
+			for (int j = col + 1; j > 0; j -= j & -j)
 				ret += bit[i][j];
 		return ret;
 	}
 
-	ValueType sum(int top, int left, int bottom, int right) {
-		return sum(bottom, right) - sum(bottom, left - 1) - sum(top - 1, right) + sum(top - 1, left - 1);
+	ValueType sum(int bottomRow, int leftCol, int topRow, int rightCol) {
+		return sum(topRow, rightCol) - sum(topRow, leftCol - 1) - sum(bottomRow - 1, rightCol) + sum(bottomRow - 1, leftCol - 1);
 	}
 };
