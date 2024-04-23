18b2a801277c390cf1959a6a51372991cf29fa37
Facundo Galan <facundogalan91@gmail.com>
2024-04-22 22:55:55 -0300

    Update floyd warshall

--- algorithms/graph/floyd_warshall.cpp
+++ algorithms/graph/floyd_warshall.cpp
@@ -32,13 +32,15 @@ void floyd_warshall_with_path(vector<vector<WeightType>>& graph, vector<vector<i
 					paths[i][j] = paths[k][j];
 				}
 }
+
 void buildPath(const vector<vector<int>>& paths, vector<int>& path, int i, int j) {
 	if (i != j) buildPath(paths, path, i, paths[i][j]);
 	path.push_back(j);
 }
 
 /** Transitive Closure */
-template<> void floyd_warshall(vector<vector<bool>>& graph) {
+template<>
+void floyd_warshall(vector<vector<bool>>& graph) {
 	int n = graph.size();
 	for (int k = 0; k < n; k++)
 		for (int i = 0; i < n; i++)
@@ -51,7 +53,7 @@ int main() {
 	// Unused edges should be initialized with something about (max_int / 2)
 	vector<vector<int>> graph(n, vector<int>(n, 1e9));
 	// graph[i][i] is the distance between vertex i and itself
-	for (int i=0; i < n; i++) graph[i][i] = 0;
+	for (int i = 0; i < n; i++) graph[i][i] = 0;
 	// This is the same node as in dijkstra.cpp
 	graph[0][2] = 1, graph[0][1] = 4, graph[1][0] = 4, graph[1][3] = 2, graph[1][5] = 1, graph[1][4] = 3;
 	graph[2][0] = 1, graph[2][3] = 1, graph[3][2] = 1, graph[3][1] = 2, graph[3][5] = 3, graph[4][1] = 3;
@@ -61,5 +63,5 @@ int main() {
 
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
-			cout << "Minimum distance from "<<i<<" to " << j << ": " << graph[i][j] << endl;
+			cout << "Minimum distance from " << i << " to " << j << ": " << graph[i][j] << endl;
 }
